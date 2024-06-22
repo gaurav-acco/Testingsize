b88eb1a5e397ad342eebe3a6b6df6dadba7f518c
gaurav-acco <gaurav-acco>
2024-06-22 16:33:02 +0530

    jj

--- Post 2.sql
+++ Post 2.sql
@@ -2,7 +2,7 @@
 -- ER/Studio Data Architect SQL Code Generation
 -- Project :      DATA MODEL
 --
--- Date Created : Saturday, June 22, 2024 16:32:39
+-- Date Created : Saturday, June 22, 2024 16:33:02
 -- Target DBMS : PostgreSQL 10.x-12.x
 --
 
@@ -34,14 +34,13 @@ CREATE TABLE "Entity2"(
 
 
 -- 
--- TABLE: "Entity4" 
+-- TABLE: "Entity3" 
 --
 
-CREATE TABLE "Entity4"(
-    ss    character(10)    NOT NULL,
-    w     character(10),
+CREATE TABLE "Entity3"(
     e     character(10),
-    PRIMARY KEY (ss)
+    f     character(10),
+    ss    character(10)
 )
 ;
 
@@ -58,10 +57,10 @@ ALTER TABLE "Entity2" ADD CONSTRAINT "RefEntity11"
 
 
 -- 
--- TABLE: "Entity4" 
+-- TABLE: "Entity3" 
 --
 
-ALTER TABLE "Entity4" ADD CONSTRAINT "RefEntity23" 
+ALTER TABLE "Entity3" ADD CONSTRAINT "RefEntity22" 
     FOREIGN KEY (ss)
     REFERENCES "Entity2"(ss)
 ;
