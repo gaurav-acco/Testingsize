3adbd0e995d7793e9a87e2ed9bbe4070761baf67
gaurav-acco <gaurav-acco>
2024-06-24 15:24:09 +0530

    ff

---
+++ post1.sql
@@ -0,0 +1,22 @@
+--
+-- ER/Studio Data Architect SQL Code Generation
+-- Project :      simpleModel1.DM1
+--
+-- Date Created : Monday, June 24, 2024 15:24:09
+-- Target DBMS : PostgreSQL 10.x-12.x
+--
+
+-- 
+-- TABLE: "Entity1" 
+--
+
+CREATE TABLE "Entity1"(
+    z     character(10),
+    a     character(10),
+    b     character(10),
+    cd    character(10)
+)
+;
+
+
+
